bd7aef49a582157bdec77af8f8a9bba8b7aee539
Arun Dhanjal <arundhanjal@hotmail.com>
2025-10-14 09:42:35 +0100

    Add excerpt and updated_on fields to Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
@@ -90,7 +90,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
+    "https://127.0.0.1",
     "https://*.herokuapp.com"
 ]
 
